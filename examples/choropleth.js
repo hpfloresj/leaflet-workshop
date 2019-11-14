5fa50bdde94c4e588f5a2dfb42616244ea1d8c12
Hugo Flores <hflores@proveedor.intercorp.com.pe>
2019-11-13 23:17:26 -0500

    Add ESRI World Imagery

---
+++ examples/choropleth.js
@@ -0,0 +1,6 @@
+
+var esri_WorldImagery = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+}).bringToBack();
+
+layersCtrl.addBaseLayer(esri_WorldImagery,'ESRI World Imagery');
