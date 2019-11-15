98f0998600ae1fb117e8b9e0131f2886e5d1612a
Hugo Flores <hflores@proveedor.intercorp.com.pe>
2019-11-15 11:44:10 -0500

    Feature implement done!

--- examples/js/choropleth.js
+++ examples/js/choropleth.js
@@ -1,63 +1,23 @@
 
-//var info = L.control({position: 'topright'});
+// GeoJSON layer to map
+addGeoJsonToMap(map, 'data/choropleth_square.json', 'Choropleth families', true, null, style, popup);
 
-addGeoJsonToMap(map, 'data/choropleth_square.json', 'Choropleth families', true, null, style, null);
+// Legend Custom Leaflet Control*/
+var legend = L.control({ position: 'bottomright' });
 
-/****************** Style *********************/
-/**
- * Add some color.
- * Now we need to color por square accoording to thir families number.  
- */
-function getColor(d) {
-    return d > 60   ? '#FC4E2A' :
-           d > 50   ? '#FD8D3C' :
-           d > 30   ? '#FEB24C' :
-           d > 20   ? '#800026' :
-           d > 10   ? '#FED976' :
-                      '#FFEDA0';
-}
+legend.onAdd = function (map) {
+    var div = L.DomUtil.create('div', 'info legend');
+    var grades = [0, 10, 20, 30, 50, 60];
+        
+    div.innerHTML += '<h4>Families/Square</h4>';
 
-/**
- * We define a style for our GeoJSON layer, so that it's fillcolor depends on 
- * feature.properties.families
- * @param {*} feature 
- */
-function style(feature) {
-    return {
-        fillColor: getColor(feature.properties.families),
-        weight: 2,
-        opacity: 1,
-        color: 'white',
-        dashArray: '3',
-        fillOpacity: 0.7
-    };
-}
-
-/****************** Add interaction *********************/
-function highlightFeature(e) {
-    var layer = e.target;
-
-    layer.setStyle({
-        weight: 5,
-        color: '#666',
-        dashArray: '',
-        fillOpacity: 0.7
-    });
-
-    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
-        layer.bringToFront();
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+            grades[i] + (grades[i + 1] ? ' - ' + grades[i + 1] + ' <br>' : ' +');
     }
 
-    // Todo for update info
-}
+    return div;
+};
 
-function resetHighlight(e) {
-    geojson.resetStyle(e.target);
-}
-
-
-
-/****************** Add info *********************/
-
-
-/****************** Add legend *********************/
+legend.addTo(map);
