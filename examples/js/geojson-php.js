e94b26cb618ea20c5dbf8884b71da8ed48a9b765
Hugo Flores <hpfloresj@gmail.com>
2019-11-18 22:46:33 -0500

    Add get feature for point

--- examples/js/geojson-php.js
+++ examples/js/geojson-php.js
@@ -1,23 +1,31 @@
 var popup = L.popup();
 var lgeojson;
-var url = "http://localhost:8083/get_features.php";
+var geojsonAPI = "http://localhost:8083/get_features.php";
 
 function onMapClick(e) {
-  $.getJSON(url, {
+  console.log("lat: " + e.latlng.lat);
+  console.log("lng: " + e.latlng.lng);
+  $.getJSON(geojsonAPI, {
     lat: e.latlng.lat,
     lng: e.latlng.lng
   }).done(function(data) {
+    console.log(data);
     var popupContent = "";
     if (lgeojson != null) {
       map.removeLayer(lgeojson);
     }
     lgeojson = L.geoJSON(data, {
       onEachFeature: function(feature, layer) {
-        popupContent += "hola";
+        popupContent +=
+          "Block with id table: " +
+          feature.properties.gid +
+          "<br>" +
+          "Code catastrade: " +
+          feature.properties.cod_lote;
       },
       style: {
         color: "blue",
-        weight: 10
+        weight: 2
       }
     });
     popup
---
+++ php/config.inc.php
@@ -0,0 +1,3 @@
+<?php
+define("DSN", "host=192.168.80.131 dbname=geomap user=geomapuser port=5432 password=Geomap2019");
+?>
